4303868e2d9068624ab02a4c90243b522121ca1b
vampiree007 <vikrantgupta.psit@gmail.com>
2020-08-15 21:00:52 +0530

    refactored code will implement security measures now

--- index.js
+++ index.js
@@ -9,6 +9,12 @@ const config = require("./server/config/keys");
 //   .then(() => console.log('MongoDB Connected...'))
 //   .catch(err => console.log(err));
 
+process.on('uncaughtException', err => {
+  console.log('UNCAUGHT EXCEPTION! 💥 Shutting down...');
+  console.log(err.name, err.message);
+  process.exit(1);
+});
+
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
@@ -31,4 +37,13 @@ const port = process.env.PORT || 5000;
 
 app.listen(port, () => {
   console.log(`Server Running at ${port}`)
+});
+
+
+process.on('unhandledRejection', err => {
+  console.log('UNHANDLED REJECTION! 💥 Shutting down...');
+  console.log(err.name, err.message);
+  server.close(() => {
+    process.exit(1);
+  });
 });
