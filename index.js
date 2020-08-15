6cef2392e7afe36bf9b4cff93bb33eb093db309a
vampiree007 <vikrantgupta.psit@gmail.com>
2020-08-16 03:41:19 +0530

    final refraction done

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
 const express = require("express");
 const path = require("path");
 const bodyParser = require("body-parser");
+
+// Security Imports here
+const helmet = require('helmet');
+const mongoSanitize = require('express-mongo-sanitize');
+const rateLimit = require('express-rate-limit');
+const xss = require('xss-clean');
+
 const app = express();
+
+//configuring Path to Config.env
 require('dotenv').config()
 
 // Handling uncaught Exception and gracefully shutting down server in Case
@@ -14,6 +23,24 @@ process.on('uncaughtException', err => {
 // Body Parsing Has been handled in below to lines, fetching incoming req.body
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+
+// 1) GLOBAL MIDDLEWARES
+// Set security HTTP headers
+app.use(helmet());
+
+// Limit requests from same API
+const limiter = rateLimit({
+  max: 100,
+  windowMs: 60 * 60 * 1000,
+  message: 'Too many requests from this IP, please try again in an hour!'
+});
+app.use('/api', limiter);
+
+// Data sanitization against NoSQL query injection
+app.use(mongoSanitize());
+
+// Data sanitization against XSS
+app.use(xss());
 
 // Handling Incoming Requests
 app.use('/api/dialogflow', require('./server/routes/dialogflow'));
